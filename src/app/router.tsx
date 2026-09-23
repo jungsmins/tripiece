32a4baf4f612b548803006ddb2b52752b543c443
jungsmins <jungsmins00@gmail.com>
2026-09-24 01:30:49 +0900

    feat: Plan, NotFound 페이지 라우팅 추가

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,20 @@
 import { Routes, Route } from 'react-router';
 import Home from '../pages/Home';
+import Plan from '../pages/Plan';
+import NotFound from '@/pages/NotFound';
+
+const ROUTES = {
+  HOME: '/',
+  NOT_FOUND: '*',
+  PLAN: '/plan/:id',
+};
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.PLAN} element={<Plan />} />
+      <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
     </Routes>
   );
 }
